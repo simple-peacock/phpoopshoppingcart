ce557fe5b1c214785da2208336c78cb2c8806372
simple-peacock <peter@simplepeacock.com>
2015-03-09 01:09:46 +1100

    Added footer with link to github.

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
-  	</div><!-- close main container div -->
-  
+        <!-- begin footer -->
+        <footer class="text-center top-buffer-large">
+
+            <img src="img/github-icon.png" alt="github">
+            <p><a href="https://github.com/simple-peacock/phpoopshoppingcart">View source on GitHub</a></p>
+
+        </footer>
+        <!-- end footer -->
+
+    </div><!-- close main container div -->
+
+    <!-- JavaScript includes -->
   	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-  
   	<script type="text/javascript" src="js/main.js"></script>
 
 	</body>
