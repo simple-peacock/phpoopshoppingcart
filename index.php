9e2c2dea13ca5908abd05f7173b3803b8a9c9f01
Peter <peter@simplepeacock.com>
2015-03-22 13:55:19 +1100

    Removed to do items from the index page.

--- index.php
+++ index.php
@@ -27,21 +27,6 @@
         <p><a class="btn btn-primary" href="https://github.com/simple-peacock/phpoopshoppingcart" role="button">Go To GitHub</a></p>
     </div>
 
-	<h2>TODO</h2>
-
-	<ul>
-		<li>messaging system, show totals - Done</li>
-		<li>improve cart display page - Done</li>
-		<li>ability to remove items - Done</li>
-		<li>show number of items in cart in the navigation - Done</li>
-		<li>create an Item class?</li>
-		<li>ability to update quantity?</li>
-		<li>autoloader? - Done</li>
-		<li>add some images to our store - Done</li>
-		<li>name spacing - Done</li>
-        <li>add continue shopping and checkout buttons - Done</li>
-	</ul>
-
 <?php
 	include('footer.php');
 ?>
