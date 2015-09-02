78e2970a64ddd5f8130d557e1444ca7fcf115775
Peter <peter@simplepeacock.com>
2015-09-02 11:28:32 +0000

    Small tweaks to database dump

--- sample_database_dump.sql
+++ sample_database_dump.sql
@@ -1,6 +1,4 @@
--- Adminer 4.2.0 MySQL dump
-
-SET NAMES utf8mb4;
+SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
@@ -26,5 +24,3 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `shipping`, `inven
 (3,	'Garmin Vivofit',	'Move Bar - Motivates you to be active throughout the day by displaying a red move bar after one hour of inactivity. Additional segments light up for every 15 minutes of inactivity. Just walk for a couple of minutes to reset. 24/7 Wearable - Stylish, comfortable and water resistant2 wristband that is always on and ready to go. Long battery life - Stays on for more than a year without having to change the battery. Easy-to-read display - View the time of day and your stats right on your wrist.',	99,	9.99,	9,	'garmin_vivofit.jpg'),
 (4,	'Samsung UN65HU7250 Curved 65-Inch 4K Ultra HD 120H',	'Refresh Rate: 120Hz (Native); Clear Motion Rate 960 (Effective) Backlight: LED (Edge-Lit w/Local Dimming) Smart Functionality: Yes - Voice Control Dimensions (W x H x D): TV without stand: 57.1\'\' x 33.1\" x 5.3\'\', TV with stand: 57.1\'\' x 34.7\'\' x 11.8\'\' Inputs: 4 HDMI, 3 USB, 1 Component In, 1 Composite In Accessories Included: Smart Remote Control',	2199,	49.99,	10,	'samsung_smart_led_tv.jpg'),
 (5,	'Sony Alpha a7S Digital Camera',	'Screen Size: 3\" Screen Features: Tiltable TFT-LCD with 921K dots Megapixels: 12.2MP Image Resolution: 4240 x 2832 Optical Zoom: Not Available',	2498,	29.99,	10,	'sony_alpha_a7s.jpg');
-
--- 2015-03-08 14:11:29
