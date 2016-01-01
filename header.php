998e42f9741473944379c4c4f1d6b5fbcd24765b
Peter <me@peterplucinski.com>
2016-01-01 08:55:34 +0000

    Removed google analytics (con't really need it in this project) and cleaned up some spacing in header.php

--- header.php
+++ header.php
@@ -9,61 +9,43 @@
 use SimplePeacock\Cart;
 
 session_start();
-
 require_once 'vendor/autoload.php';
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
   	<head>
     	<meta charset="utf-8" />
     	<title>PHP Shopping Cart</title>
-
     	<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/mystyles.css">
-
   	</head>
-
   	<body>
+    	<!-- main body container -->
+    	<div class="container">
+            <!-- begin navigation -->
+            <nav class="navbar navbar-inverse top-buffer-small">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li class='<?php if($page == "home") { echo "active"; } ?>'><a href="index.php">Home</a></li>
+                        <li class='<?php if($page == "products") { echo "active"; } ?>'><a href="products.php">Products</a></li>
+                        <li class='<?php if($page == "cart") { echo "active"; } ?>'><a href="cart.php">Cart
 
-    <?php include_once('googleanalytics.php'); ?>  
+                        <?php
+                        /*
+                         * This code displays the number of items
+                         * currently in our shopping cart.
+                         *
+                         */
 
-	<!-- main body container -->
-	<div class="container">
+                        $cart = new Cart();
+                        $itemCount = $cart->countItems();
 
-        <nav class="navbar navbar-inverse top-buffer-small">
-            <div class="container-fluid">
-                <!-- begin navigation -->
-                <ul class="nav navbar-nav">
-                    <li class='<?php if($page == "home") { echo "active"; } ?>'><a href="index.php">Home</a></li>
-                    <li class='<?php if($page == "products") { echo "active"; } ?>'><a href="products.php">Products</a></li>
-                    <li class='<?php if($page == "cart") { echo "active"; } ?>'><a href="cart.php">Cart
-
-                    <?php
-                    /*
-                     * This code displays the number of items
-                     * currently in our shopping cart.
-                     *
-                     */
-
-                    $cart = new Cart();
-
-                    $itemCount = $cart->countItems();
-
-                    if($itemCount > 0) {
-
-                    ?>
-                            <span class="badge">
-
-                                <?php echo $itemCount; ?>
-
-                            </span>
-
-                    <?php } ?>
+                        if($itemCount > 0) {
+                        ?>
+                            <span class="badge"><?php echo $itemCount; ?></span>
+                        <?php } ?>
                         </a></li>
-
-                </ul>
-            </div>
-        </nav>
-		<!-- end of navigation -->
+                    </ul>
+                </div>
+            </nav>
+    		<!-- end of navigation -->
