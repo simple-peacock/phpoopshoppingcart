d30fca8d305666b14514bd88ed895cac3bf5de06
Peter <me@peterplucinski.com>
2016-01-01 09:17:24 +0000

    Cleaned up some spacing in footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,15 @@
-        <!-- begin footer -->
-        <footer class="text-center top-buffer-large">
+            <!-- begin footer -->
+            <footer class="text-center top-buffer-large">
+                <img src="img/github-icon.png" alt="github">
+                <p><a href="https://github.com/simple-peacock/phpoopshoppingcart">View source on GitHub</a></p>
+            </footer>
+            <!-- end footer -->
 
-            <img src="img/github-icon.png" alt="github">
-            <p><a href="https://github.com/simple-peacock/phpoopshoppingcart">View source on GitHub</a></p>
+        </div>
+        <!-- close main container div -->
 
-        </footer>
-        <!-- end footer -->
-
-    </div><!-- close main container div -->
-
-    <!-- JavaScript includes -->
-  	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-  	<script type="text/javascript" src="js/main.js"></script>
-
+        <!-- JavaScript includes -->
+      	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+      	<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
